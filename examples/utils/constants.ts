874ade2fa66e70d9e1bbb29e69afbcfa2f58788e
CCP Legolas <toniya@ccpgames.com>
2026-01-18 19:19:55 +0000

    feat: add error decoder tool for Move abort codes

---
+++ examples/utils/constants.ts
@@ -0,0 +1,31 @@
+// Common constants used across all example scripts
+// Sui System Objects
+export const CLOCK_OBJECT_ID = "0x6";
+
+export const TENANT = "dev";
+
+// Change this value, if you want to create a new character
+export const GAME_CHARACTER_ID = 811877;
+
+// Location
+export const LOCATION_HASH = "0x7a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b7a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b";
+export const PROOF =
+    "0x93d3209c7f138aded41dcb008d066ae872ed558bd8dcb562da47d4ef78295333202d7d52ab5f8e8824e3e8066c0b7458f84e326c5d77b30254c69d807586a7b00000000000000000000000000000000000000000000000000000000000000000207a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b7a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b0000000000000000000000000000000000000000000000000000000000000000207a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b7a8f3b2e9c4d1a6f5e8b2d9c3f7a1e5b010000000000000000d16ff3c69b010000610098cb7f8b9eba10d42b2c73be4beb719c745aeecbd118144bf3e9e35ca74c57f674399da455c42d40d56e77e85d8c650059b743b568be9abab1bfb16a4784b80ba94e21ea26cc336019c11a5e10c4b39160188dda0f6b4bfe198dd689db8f3df9";
+
+// Network Node Defaults
+export const NWN_TYPE_ID = 555n;
+// Change this value, to create a new network node
+export const NWN_ITEM_ID = 555000001n;
+
+// Assembly Defaults
+export const ASSEMBLY_TYPE_ID = 87119n;
+// Change this value, to create a new assembly
+export const ASSEMBLY_ITEM_ID = 9999000005n;
+
+// Storage Unit Defaults
+export const STORAGE_A_TYPE_ID = 88082n;
+export const STORAGE_A_ITEM_ID = 888800005n;
+
+// Item Default
+export const ITEM_A_TYPE_ID = 446n;
+export const ITEM_A_ITEM_ID = 444000001n;
